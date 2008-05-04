cce23e6d5b3a5a9956b0c1ebdcab07b5e7b583f6
ralph.greschner <ralph.greschner@1fb93824-844b-0410-90e3-eb52d5e16dc5>
2008-05-04 18:15:20 +0000

    Rewrite of Windows Client - VS 2005 - dedicated business model - utilizes MVC pattern and Controller

---
+++ trunk/ASP.NET/Zeiterfassung.NET/Program.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Zeiterfassung.NET
+{
+
+    /*
+     * Author: Ralph Greschner
+     * Date: 04/05/2008
+     * 
+     * Controller class for the application (MVC),
+     * should land in separate file (
+     */
+
+    class MainController
+    {
+        private LoginForm loginForm;
+        private MainForm mainForm;
+        private LibZES.Backend backend;
+
+        public MainController()
+        {
+            // Create business model
+            backend = new LibZES.Backend();
+
+            // Create views
+            loginForm = new LoginForm();
+            mainForm = new MainForm();
+
+            // Supply business models for Forms (dirty hack)
+            loginForm.backend = this.backend;
+            mainForm.backend = this.backend;
+            SubscribeAll();
+        }
+        private void SetViewSubscriptionsOnModel()
+        {
+            backend.delLoginStatusChanged += new Action<LibZES.StatusCode>(loginForm.LoginStatusChanged);
+            backend.delLoginStatusChanged += new Action<LibZES.StatusCode>(this.OnLoginStatusChanged);
+
+        }
+        private void SetModelSubscriptionsOnView()
+        {
+            loginForm.delExitApp += new Action<int>(this.OnExitApp);
+            mainForm.delExitApp += new Action<int>(this.OnExitApp);
+        }
+        public void SubscribeAll()
+        {
+            SetViewSubscriptionsOnModel();
+            SetModelSubscriptionsOnView();
+
+        }
+        public void OnExitApp(int n)
+        {
+            System.Windows.Forms.MessageBox.Show("blubb");
+            Application.Exit();
+        }
+        public void OnLoginStatusChanged(LibZES.StatusCode status)
+        {
+            switch (status)
+            {
+                case LibZES.StatusCode.LOGIN_SUCCESSFULL:
+                    mainForm.Show();
+                    break;
+                case LibZES.StatusCode.LOGOUT_SUCCESSFULL:
+                    mainForm.Hide();
+                    break;
+            }
+        }
+
+        // Start of application flow
+        public void Run()
+        {
+            loginForm.Show();
+        }
+
+    }
+    
+    static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            MainController c = new MainController();
+            c.Run();
+
+            Application.Run();
+        }
+    }
+}
---
+++ trunk/ASP.NET/Zeiterfassung.NET/backend_interface.cs
@@ -0,0 +1,147 @@
+/*
+ * backend_interface.cs
+ * 
+ * Author: Ralph Greschner
+ * Date: 04/05/2008
+ * 
+ * Public Interfaces for business model (back-end)
+ */
+
+using System;
+
+namespace LibZES
+{
+    public enum StatusCode
+    {
+        DB_CONNECTION_SUCCESSFULL,
+        DB_CONNECTION_FAILED,
+        LOGIN_FAILED,
+        LOGIN_SUCCESSFULL,
+        LOGOUT_SUCCESSFULL,
+        USER_VALIDATION_FAILED,
+        USER_VALIDATION_SUCCESSFULL
+    }
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class Backend
+	{
+
+        public System.Action<StatusCode> delLoginStatusChanged;
+        public System.Action<StatusCode> delDatabaseConnectionStateChanged;
+
+		public Backend()
+        {
+        	SetDefaultOptions();
+        }
+		
+		public string BackendInfo
+		{
+			get
+			{
+				return "Backend MSSQL 0.1";
+			}
+		}
+
+
+		public StatusCode Login(string p_username, string p_password)
+        {
+            StatusCode status = StatusCode.LOGIN_FAILED;
+			if (userLoggedIn)
+				Logout();
+
+        	if (ConnectDb() != StatusCode.DB_CONNECTION_SUCCESSFULL)
+            {
+                status = StatusCode.DB_CONNECTION_FAILED;
+            } else
+            if (ValidateUserCredentials(p_username, p_password) != StatusCode.USER_VALIDATION_SUCCESSFULL)
+            {
+                status = StatusCode.USER_VALIDATION_FAILED;
+            }
+            else
+            {
+                username = p_username;
+                password = p_password;
+                userid = GetUserId(username);
+                userLoggedIn = true;
+                status = StatusCode.LOGIN_SUCCESSFULL;
+            }
+            if (delLoginStatusChanged != null) delLoginStatusChanged(status);
+            return status;
+		}
+
+        public StatusCode Logout()
+        {
+            con.Close();
+            userLoggedIn = false;
+            username = null;
+            password = null;
+            if (delLoginStatusChanged != null) delLoginStatusChanged(StatusCode.LOGOUT_SUCCESSFULL);
+            return StatusCode.LOGOUT_SUCCESSFULL;
+
+        }
+        
+        public int GetUserId(string p_username)
+		{
+        	int erg = 0;
+			/*
+			con.Open();
+			
+			System.Data.Common.DbCommand cmd = con.CreateCommand();
+			cmd.CommandText = "SELECT MId FROM dbo.Mitarbeiter WHERE an LIKE \"p_username\";";
+			int erg = (int)cmd.ExecuteScalar();
+			cmd.Dispose();
+			con.Close();*/
+			return erg;
+		}
+        
+        public void SetOption(string name, string value)
+        {
+        	string name_key = name.ToLower();
+        	options[name_key] = value;
+        }
+        
+        public string GetOption(string name)
+        {
+        	string name_key = name.ToLower();
+        	if (!options.Contains(name_key))
+        		return "";
+        	return (string)(options[name_key]);
+        }
+        public void SetDefaultOptions()
+        {
+        	options.Clear();
+        }
+
+        public void LoadOptionsFromFile(string fn)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(fn);
+            string line = null;
+            string[] line_split;
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("="))
+                {
+                    line_split = line.Split("=".ToCharArray());
+                    SetOption(line_split[0],line_split[1]);
+                }
+
+                
+            }
+            file.Close();
+        }
+        
+        public bool IsUserLoggedIn {
+			get { return userLoggedIn; }
+		}
+        public String QueryTest1()
+        {
+            System.Data.Common.DbCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT Namen FROM dbo.Mitarbeiter WHERE MId = 1";
+            String erg = (String)cmd.ExecuteScalar();
+            return erg;
+
+        }
+
+	}
+}
