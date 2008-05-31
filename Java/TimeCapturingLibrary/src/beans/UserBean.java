6fa41bb80bbed4233ba363f0c81d6ea204c53f1f
manuwo <manuwo@1fb93824-844b-0410-90e3-eb52d5e16dc5>
2008-05-31 11:21:34 +0000

    initialer Import

---
+++ Java/TimeCapturingLibrary/src/beans/UserBean.java
@@ -0,0 +1,70 @@
+package beans;
+
+/**
+ *
+ * @author manuel
+ */
+public class UserBean
+{
+
+    private String mid;
+    private String firstname;
+    private String name;
+    private String username;
+    private String password;
+
+    public UserBean()
+    {
+    }
+
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+
+    public String getMid()
+    {
+        return mid;
+    }
+
+    public void setMid(String mid)
+    {
+        this.mid = mid;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+}
