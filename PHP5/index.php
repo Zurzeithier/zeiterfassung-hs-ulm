ec62cd9f8210812010d5da8a3f0465b13b335bf1
patrick.kracht <patrick.kracht@1fb93824-844b-0410-90e3-eb52d5e16dc5>
2008-05-31 00:37:26 +0000

    class Email added; class Session basic functions; missing login verification and authentication (MD5?)

--- PHP5/index.php
+++ PHP5/index.php
@@ -3,11 +3,15 @@
 /**
  * The mainprogram
  *
- * Copyright 2008   Patrick Kracht <patrick.kracht@googlemail.com>
- *                  Thorsten Moll  <thorsten.moll@googlemail.com>
+ * @copyright 2008  Patrick Kracht <patrick.kracht@googlemail.com>
+ * @copyright 2008  Thorsten Moll  <thorsten.moll@googlemail.com>
+ * 
+ * @filesource      http://code.google.com/p/zeiterfassung-hs-ulm/source/browse/trunk/PHP5/
  *
  * @author          Patrick Kracht <patrick.kracht@googlemail.com>
  *                  Thorsten Moll  <thorsten.moll@googlemail.com>
+ * 
+ * @see             http://www.omega2k.de/~omega2k/WEBE/
  * 
  * @version         0.3_alpha         
  */
@@ -17,7 +21,7 @@
  * INIT STARTS HERE
  */
  
- include( "./functions.php" );
+ require_once "./functions.php" ;
  __init_main();
  
  $output = "";
@@ -27,20 +31,25 @@
  */
  
  // create needed objects
- $MYS = new MySql; //TEST ONLY
+ //$MYS = new MySql; //TEST ONLY
+ $SID = new Session;
  $SQL = new MsSql;
  $TPL = new Template;
+ //$EML = new Email( "index.html" );
  
  // load template index.html
  $TPL->load( "index.html" );
  
  // do some sql-queries...
- $result = $MYS->query( "SELECT * FROM Mitarbeiter;" ); //TEST ONLY
+ //$result = $MYS->query( "SELECT * FROM Mitarbeiter;" ); //TEST ONLY
  $result = $SQL->query( "SELECT * FROM Mitarbeiter;" );
  while( $row = $SQL->fetch_array( $result ) )
  {
   $output .= "MSSQL : " . nl2br( print_r( $row, true ) ); 
  }
+ 
+ // DEBUG
+ print_r( $_SESSION );
  
  // assing content to specific replacement vars
  $TPL->assign( "index.html", "{{OUTPUT}}", $output );
