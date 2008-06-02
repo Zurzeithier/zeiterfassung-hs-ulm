167a4171712a0c203b8517d3f8677d19e6d84d4a
manuwo <manuwo@1fb93824-844b-0410-90e3-eb52d5e16dc5>
2008-06-02 21:24:12 +0000

    changed

--- Java/TimeCapturing/src/java/timecapturing/Login.java
+++ Java/TimeCapturing/src/java/timecapturing/Login.java
@@ -218,7 +218,7 @@ public class Login extends AbstractPageBean
 
         if (UserController.getInstance().LoginUser(userBean))
         {
-            return null;
+            return "loginSucessfull";
         }
         else
         {
