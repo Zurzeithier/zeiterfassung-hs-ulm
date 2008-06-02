d884a643100fa26769821938b71441ab7ff27939
manuwo <manuwo@1fb93824-844b-0410-90e3-eb52d5e16dc5>
2008-06-02 20:26:25 +0000

    changed

--- trunk/Java/TimeCapturingLibrary/src/database/ConnectionDataProvider.java
+++ trunk/Java/TimeCapturingLibrary/src/database/ConnectionDataProvider.java
@@ -1,5 +1,17 @@
 package database;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author manuel
@@ -7,9 +19,9 @@ package database;
 public class ConnectionDataProvider
 {
 
-    private static String m_Adress = null;
-    private static String m_Username = null;
-    private static String m_Password = null;
+    private static String m_Adress = "";
+    private static String m_Username = "";
+    private static String m_Password = "";
     private static boolean m_Initialized = false;
 
     public static String getAdress()
@@ -28,7 +40,7 @@ public class ConnectionDataProvider
         {
             initialize();
         }
-        
+
         return m_Password;
     }
 
@@ -38,15 +50,60 @@ public class ConnectionDataProvider
         {
             initialize();
         }
-        
+
         return m_Username;
     }
 
     private static void initialize()
     {
-        m_Adress = "www.illertech.net;databaseName=Zeiterfassung";
-        m_Username = "sa";
-        m_Password = "odysee2001";
+        try
+        {
+            File domFile = new File("setup/dbConnect.xml");
+            if (domFile.exists())
+            {
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document document = builder.parse(domFile);
+                Node rootNode = document.getDocumentElement();
+
+                NodeList childs = rootNode.getChildNodes();
+                Node nodeChild = null;
+
+                for (int j = 0; j < childs.getLength(); j++)
+                {
+                    nodeChild = childs.item(j);
+                    if (nodeChild != null)
+                    {
+                        if ("url".equals(nodeChild.getNodeName()))
+                        {
+                            m_Adress = nodeChild.getTextContent();
+                        }
+                        else if ("user".equals(nodeChild.getNodeName()))
+                        {
+                            m_Username = nodeChild.getTextContent();
+                        }
+                        else if ("pwd".equals(nodeChild.getNodeName()))
+                        {
+                            m_Username = nodeChild.getTextContent();
+                        }
+                    }
+                }
+                
+                m_Initialized = true;
+            }
+        }
+        catch (ParserConfigurationException ex)
+        {
+            Logger.getLogger(ConnectionDataProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (SAXException ex)
+        {
+            Logger.getLogger(ConnectionDataProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(ConnectionDataProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
