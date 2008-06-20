f29e109f65f82946fde28292120a6128dc84a630
patrick.kracht <patrick.kracht@1fb93824-844b-0410-90e3-eb52d5e16dc5>
2008-06-20 12:27:06 +0000

    controller modified;

--- trunk/TimeRecording/index.php
+++ trunk/TimeRecording/index.php
@@ -21,102 +21,11 @@ require_once "./functions.php";
 
 
 // create controller and check for actions to do
-$Controller =& new Controller();
+$Controller =& new Controller;
 $Controller->switch_actions();
+$Controller->prepare_templates();
+$Controller->create_menu();
+$Controller->create_page();
 
 
-// temporary update and preload all templates (for development purpose)
-$_SESSION["HTML"]->import();
-$_SESSION["HTML"]->preload();
-
-
-// ************************* pages for users ***********************
-if ($_SESSION["CLIENT"]->is_user())
-	{
-		// create top menu
-		$_SESSION["HTML"]->menu_insert_entry("Abmelden", "./?action=logout", "Q");
-		$_SESSION["HTML"]->menu_insert_spacer();
-		$_SESSION["HTML"]->menu_insert_entry("&Uuml;bersicht", "./?page=home", "H", ($_SESSION["_PageID.current"]=="home"));
-		$_SESSION["HTML"]->menu_insert_entry("Mitarbeiter", "./?page=users", "E", ($_SESSION["_PageID.current"]=="users"));
-		$_SESSION["HTML"]->menu_insert_entry("Einstellungen", "./?page=setup", "E", ($_SESSION["_PageID.current"]=="setup"));
-		$_SESSION["HTML"]->menu_insert_spacer();
-		$_SESSION["HTML"]->menu_insert_entry("[<u>KOMMEN</u>]", "./?action=book&amp;id=1", "K");
-		$_SESSION["HTML"]->menu_insert_entry("[<u>GEHEN</u>]", "./?action=book&amp;id=0", "G");
-		
-		// call page specific methods
-		switch ($_SESSION["_PageID.current"])
-			{
-			case "users":
-				// load template
-				$_SESSION["HTML"]->load("users.html");
-				
-				// list users
-				$Controller->show_user_table();
-				
-				// send page to browser
-				$_SESSION["HTML"]->output("users.html");
-				break;
-			case "setup":
-				// load template
-				$_SESSION["HTML"]->load("setup.html");
-				
-				//TODO ALL
-				
-				// send page to browser
-				$_SESSION["HTML"]->output("setup.html");
-				break;
-			case "home":
-			default:
-				// load template
-				$_SESSION["HTML"]->load("index.html");
-				
-				$_SESSION["HTML"]->assign("index.html", "<!--MID-->", $_SESSION["_UserData"]["mid"]);
-				$_SESSION["HTML"]->assign("index.html", "<!--FIRST_NAME-->", $_SESSION["_UserData"]["firstname"]);
-				$_SESSION["HTML"]->assign("index.html", "<!--LAST_NAME-->", $_SESSION["_UserData"]["lastname"]);
-				$_SESSION["HTML"]->assign("index.html", "<!--LOGIN_NAME-->", $_SESSION["_UserData"]["email"]);
-				$_SESSION["HTML"]->assign("index.html", "<!--IP-->", $_SESSION["_UserData"]["ip"]);
-				$_SESSION["HTML"]->assign("index.html", "<!--GROUP_NAME-->", $_SESSION["_UserData"]["groupname"]);
-				
-				// list last bookings
-				$Controller->show_last_bookings();
-				
-				// summary calculation and assignment for current week and month
-				$array = $Controller->get_booking_sums("WEEK");
-				$_SESSION["HTML"]->assign("index.html", "<!--SUM_WEEK_NOW-->",$array["Stunden"]);
-				$_SESSION["HTML"]->assign("index.html", "{{SUM_WEEK_NOW_TL}}",$array["Von"]." - ".$array["Bis"]);
-				$array = $Controller->get_booking_sums("MONTH");
-				$_SESSION["HTML"]->assign("index.html", "<!--SUM_MONTH_NOW-->",$array["Stunden"]);
-				$_SESSION["HTML"]->assign("index.html", "{{SUM_MONTH_NOW_TL}}",$array["Von"]." - ".$array["Bis"]);
-				
-				// summary calculation and assignment for last week and month
-				$array = $Controller->get_booking_sums("WEEK", 1);
-				$_SESSION["HTML"]->assign("index.html", "<!--SUM_WEEK_LAST-->",$array["Stunden"]);
-				$_SESSION["HTML"]->assign("index.html", "{{SUM_WEEK_LAST_TL}}",$array["Von"]." - ".$array["Bis"]);
-				$array = $Controller->get_booking_sums("MONTH", 1);
-				$_SESSION["HTML"]->assign("index.html", "<!--SUM_MONTH_LAST-->",$array["Stunden"]);
-				$_SESSION["HTML"]->assign("index.html", "{{SUM_MONTH_LAST_TL}}",$array["Von"]." - ".$array["Bis"]);
-				
-				// send page to browser
-				$_SESSION["HTML"]->output("index.html");
-				break;
-			}
-	}
-// ***************** page for guests (force login) *****************
-else
-	{
-		if ($_SESSION["_PageID.current"] == "passwd")
-			{
-				// load template
-				$_SESSION["HTML"]->load("passwd.html");
-				// send page to browser
-				$_SESSION["HTML"]->output("passwd.html");
-			}
-		else
-			{
-				// load template
-				$_SESSION["HTML"]->load("login.html");
-				// send page to browser
-				$_SESSION["HTML"]->output("login.html");
-			}
-	}
 ?>
